1a9d872a04adc0576441f6259fcd18026db0300c
bhubanbhaskar <bhubanbhaskar91@gmail.com>
2023-09-16 20:34:32 +0530

    This is second Commit
    This is second Commit

--- Main.java
+++ Main.java
@@ -1,8 +1,11 @@
+import java.time.Instant;
+import java.util.Calendar;
 import java.util.Date;
 
 public class Main {
     public static void main(String args[]){
      System.out.println("This is first Java Jenkins code");
         System.out.println("Current date is "+ new Date());
+        System.out.println("Current date is "+ Instant.now());
     }
 }
