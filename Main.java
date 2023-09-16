8f1b298714209bb9b31c067542684b0a4772f9e5
bhubanbhaskar <bhubanbhaskar91@gmail.com>
2023-09-16 20:16:30 +0530

    Initial commit

---
+++ Main.java
@@ -0,0 +1,8 @@
+import java.util.Date;
+
+public class Main {
+    public static void main(String args[]){
+     System.out.println("This is first Java Jenkins code");
+        System.out.println("Current date is "+ new Date());
+    }
+}
